11c6078b042d3d6b0426a3bf86454cea17cad7d6
iamqi <2911027255@qq.com>
2022-07-04 16:54:17 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+<h1>我是根组件</h1>
+  <router-view/>
+</template>
+<style lang="less">
+</style>
---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    count:0
+  },
+  getters: {
+
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
